0b8f413b437ca42268df3bb0f9f6c71c77d094c1
ZaheerNiazi <zaheerniazi1996@gmail.com>
2023-01-26 16:32:37 +0500

    react open ai

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Configuration, OpenAIApi } from "openai";
+
+import "./App.css";
 
 function App() {
+  const configuration = new Configuration({
+    apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+  const [prompt, setPrompt] = React.useState("");
+  const [result, setResult] = React.useState("");
+  const [loading, setLoading] = React.useState(false);
+  const handleClick = async () => {
+    setLoading(true);
+    try {
+      const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: prompt,
+        temperature: 0.5,
+        max_tokens: 100,
+      });
+      setResult(response.data.choices[0].text);
+    } catch (error) {
+      console.log(error);
+    }
+    setLoading(false);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <main className="main d-flex align-items-center justify-content-center">
+      <div className="w-2/4 mx-auto text-center">
+        <div className="form-group">
+          <textarea
+            className="form-control"
+            type="text"
+            value={prompt}
+            onChange={(e) => setPrompt(e.target.value)}
+            placeholder="Write your prompt"
+          ></textarea>
+        </div>
+        <button
+          className="btn btn-primary"
+          onClick={handleClick}
+          disabled={loading || prompt.length === 0}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {loading ? "Generating..." : "Generate"}
+        </button>
+        <div class="text-center mw-30">{result} </div>
+      </div>
+    </main>
   );
 }
 
